2d4372f6049c908a30745c3b00627abca493d265
Valentin Ghibaudo <vatghibaudo@gmail.com>
2026-03-24 19:28:34 +0100

    add test time to cycle

--- tests/test_cyclic_deformation.py
+++ tests/test_cyclic_deformation.py
@@ -1,12 +1,13 @@
 import numpy as np
 from pathlib import Path
 
-from physio import compute_respiration, deform_traces_to_cycle_template
+from physio import compute_respiration, deform_traces_to_cycle_template, compute_ecg, time_to_cycle
 
 
 # read signals
 example_folder = Path(__file__).parents[1] / 'examples'
 raw_resp = np.load(example_folder / 'resp1_airflow.npy')
+raw_ecg = np.load(example_folder / 'ecg1.npy')
 srate = 1000.
 
 
@@ -42,5 +43,15 @@ def test_deform_traces_to_cycle_template():
     assert deformed_resp.shape == cycle_points.shape
 
 
+
+def test_time_to_cycle():
+    _, resp_cycles = compute_respiration(raw_resp, srate, parameter_preset='human_airflow')
+    _ , ecg_peaks = compute_ecg(raw_ecg, srate, parameter_preset = 'human_ecg')
+    inspi_ratio = resp_cycles['cycle_ratio'].median()
+    cycle_times = resp_cycles[['inspi_time', 'expi_time', 'next_inspi_time']].values
+    rpeak_phase = time_to_cycle(ecg_peaks['peak_time'].values, cycle_times, segment_ratios=[inspi_ratio])
+
+
 if __name__ == '__main__':
-    test_deform_traces_to_cycle_template()
+    # test_deform_traces_to_cycle_template()
+    test_time_to_cycle()
