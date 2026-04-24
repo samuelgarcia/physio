d8feb333b9b5d7ba683bb71c9fa98ee056778bc7
Valentin Ghibaudo <vatghibaudo@gmail.com>
2026-04-24 09:56:05 +0200

    develop compute_resphrv_rate_period (unit bpm and ms)

--- physio/resphrv.py
+++ physio/resphrv.py
@@ -156,6 +156,210 @@ def compute_resphrv(resp_cycles, ecg_peaks, srate=100., units='bpm', limits=None
         return resphrv_cycles, cyclic_cardiac_rate
     else:
         return resphrv_cycles
+    
+def compute_resphrv_rate_period(resp_cycles, ecg_peaks, srate=100., bpm_limits=None, two_segment=True, points_per_cycle=50, return_cyclic_cardiac_rate=True, return_cyclic_cardiac_period = True):
+    """
+    RSA = Respiratory Sinus Arrhythmia (or Respiratory Heart Rate Variability / RespHRV)
+
+    Compute the RSA cycle-by-cycle : 
+      * compute instantaneous heart rate
+      * on resp cycle basis compute peak-to-trough
+
+    Also compute the cyclic deformation of the instantaneous heart rate
+
+    Parameters
+    ----------
+
+    resp_cycles : pd.DataFrame
+        DataFrame of detected respiratory cycles
+    ecg_peaks : pd.DataFrame
+        DataFrame of detected ecg R peaks
+    srate : int or float
+        Sampling rate used for interpolation to get an instantaneous heart rate vector, to compute cyclic_cardiac_rate. 
+        100 is safe for both animal and human. For human 10 also works.
+    units : str
+        bpm / Hz
+    limits : list or None
+        Limits for removing outliers. To set according to the units parameter. Ex : [30, 200] to remove heart rates (in bpm) out of this range.
+    two_segment : bool
+        True or False (default = True). Deform instantaneous heart rate by respiratory phase using one segment (inspi_time to next_inspi_time) or two segments (inspi_time to expi_time and expi_time to next_inspi_time), to compute cyclic_cardiac_rate.
+    points_per_cycle : int
+        Number of respiratory phase points per cycle, used in deform_traces_to_cycle_template() to build cyclic_cardiac_rate matrix
+    return_cyclic_cardiac_rate : bool
+        If True, returns both outputs (resphrv_cycles and cyclic_cardiac_rate), else computes and returns only resphrv_cycles and not cyclic_cardiac_rate
+
+    Returns
+    -------
+    resphrv_cycles : pd.DataFrame
+        Cycle-by-cycle features of Heart Rate dynamics. Ex : decay_amplitude gives the by-cycle peak-to-trough amplitude.
+    cyclic_cardiac_rate : nd.array
+        2D Matrix (respiratory cycle * respiratory phase) with instantaneous heart rate at each resp cycle and phase point.
+    """
+    rate_units = 'bpm'
+    period_units = 'ms'
+
+    rate_limits = bpm_limits
+
+    if not rate_limits is None:
+        period_limits = (60000 / rate_limits[0], 60000 / rate_limits[1])
+    else:
+        period_limits = None
+
+    if return_cyclic_cardiac_rate:
+        duration_s = resp_cycles['next_inspi_time'].values[-1]
+
+        times = np.arange(0,  duration_s + 1 / srate, 1 / srate)
+        instantaneous_cardiac_rate = compute_instantaneous_rate(ecg_peaks, times, limits=rate_limits, units=rate_units, interpolation_kind='linear')    
+        
+        if two_segment:
+            cycle_times = resp_cycles[['inspi_time', 'expi_time','next_inspi_time']].values
+            inspi_ratio = np.mean((cycle_times[:, 1] - cycle_times[:, 0]) / (cycle_times[:, 2] - cycle_times[:, 0]))
+            segment_ratios = [inspi_ratio]
+        else:
+            cycle_times = resp_cycles[['inspi_time', 'next_inspi_time']].values
+            segment_ratios = None
+
+        cyclic_cardiac_rate = deform_traces_to_cycle_template(instantaneous_cardiac_rate, times, cycle_times, points_per_cycle=points_per_cycle, segment_ratios=segment_ratios)
+
+    if return_cyclic_cardiac_period:
+        duration_s = resp_cycles['next_inspi_time'].values[-1]
+
+        times = np.arange(0,  duration_s + 1 / srate, 1 / srate)
+        instantaneous_cardiac_period = compute_instantaneous_rate(ecg_peaks, times, limits=period_limits, units=period_units, interpolation_kind='linear')    
+        
+        if two_segment:
+            cycle_times = resp_cycles[['inspi_time', 'expi_time','next_inspi_time']].values
+            inspi_ratio = np.mean((cycle_times[:, 1] - cycle_times[:, 0]) / (cycle_times[:, 2] - cycle_times[:, 0]))
+            segment_ratios = [inspi_ratio]
+        else:
+            cycle_times = resp_cycles[['inspi_time', 'next_inspi_time']].values
+            segment_ratios = None
+
+        cyclic_cardiac_period = deform_traces_to_cycle_template(instantaneous_cardiac_period, times, cycle_times, points_per_cycle=points_per_cycle, segment_ratios=segment_ratios)
+
+    resphrv_cycles = pd.DataFrame(index=resp_cycles.index)
+
+    n = resp_cycles.shape[0]
+
+    columns_rate=['peak_time_bpm', 'trough_time_bpm',
+             'peak_value_bpm', 'trough_value_bpm',
+             'min_max_amplitude_bpm','relative_min_max_amplitude_bpm',
+             'rising_amplitude_bpm', 'relative_rising_amplitude_bpm' ,'decay_amplitude_bpm', 'relative_decay_amplitude_bpm',
+             'rising_duration_bpm', 'decay_duration_bpm',
+             'rising_slope_bpm', 'decay_slope_bpm',
+             ]
+    columns_period=['trough_time_ms', 'peak_time_ms',
+             'trough_value_ms', 'peak_value_ms',
+             'min_max_amplitude_ms','relative_min_max_amplitude_ms',
+             'decay_amplitude_ms', 'relative_decay_amplitude_ms' ,'rising_amplitude_ms', 'relative_rising_amplitude_ms',
+             'decay_duration_ms', 'rising_duration_ms',
+             'decay_slope_ms', 'rising_slope_ms',
+             ]
+    columns = columns_rate + columns_period
+    
+    for col in columns:
+        resphrv_cycles[col] = pd.Series(dtype='float64')
+
+    ecg_peak_times = ecg_peaks['peak_time'].values
+    delta_s = np.diff(ecg_peak_times)
+    ecg_peak_times = ecg_peak_times[:-1]
+
+    hrate = 60.  / delta_s
+    hperiod = 1000. * delta_s
+    
+    if not bpm_limits is None:
+        mask = (hrate > bpm_limits[0]) & (hrate < bpm_limits[1])
+        hrate = hrate[mask]
+        hperiod = hperiod[mask]
+        ecg_peak_times = ecg_peak_times[mask]
+
+    for c, cycle in resp_cycles.iterrows():
+        t0, t1 = cycle['inspi_time'], cycle['next_inspi_time']
+
+        ind0, ind1 =  np.searchsorted(ecg_peak_times, [t0, t1])
+        if ind0 == ind1:
+            continue
+
+        ind_max_rate = np.argmax(hrate[ind0:ind1]) + ind0
+        resphrv_cycles.at[c, 'peak_time_bpm'] = ecg_peak_times[ind_max_rate]
+        resphrv_cycles.at[c, 'peak_value_bpm'] = hrate[ind_max_rate]
+        resphrv_cycles.at[c, 'trough_time_ms'] = ecg_peak_times[ind_max_rate]
+        resphrv_cycles.at[c, 'trough_value_ms'] = hperiod[ind_max_rate]
+
+        if ind1 - ind0 >= 2: # check if enough ecg peaks detected during the resp cycle to compute resphrv min max
+            max_rate, min_rate = np.max(hrate[ind0:ind1]), np.min(hrate[ind0:ind1])
+            ptp_rate = max_rate - min_rate
+            relative_ptp_rate = ptp_rate / (max_rate + min_rate)
+            resphrv_cycles.at[c, 'min_max_amplitude_bpm'] = ptp_rate
+            resphrv_cycles.at[c, 'relative_min_max_amplitude_bpm'] = relative_ptp_rate
+
+            max_period, min_period = np.max(hperiod[ind0:ind1]), np.min(hperiod[ind0:ind1])
+            ptp_period = max_period - min_period
+            relative_ptp_period = ptp_period / (max_period + min_period)
+            resphrv_cycles.at[c, 'min_max_amplitude_ms'] = ptp_period
+            resphrv_cycles.at[c, 'relative_min_max_amplitude_ms'] = relative_ptp_period
+
+
+    for c, cycle in resp_cycles.iloc[:-1].iterrows():
+        t0 = resphrv_cycles.loc[c, 'peak_time_bpm']
+        t1 = resphrv_cycles.loc[c+1, 'peak_time_bpm']
+
+        if np.isnan(t0) or np.isnan(t1):
+            continue
+
+        ind0, ind1 = np.searchsorted(ecg_peak_times, [t0, t1])
+        ind0 += 1
+        if ind0+1 >= ind1:
+            continue
+
+        ind_min_rate = np.argmin(hrate[ind0:ind1]) + ind0
+        resphrv_cycles.at[c, 'trough_time_bpm'] = ecg_peak_times[ind_min_rate]
+        resphrv_cycles.at[c, 'trough_value_bpm'] = hrate[ind_min_rate]
+        resphrv_cycles.at[c, 'peak_time_ms'] = ecg_peak_times[ind_min_rate]
+        resphrv_cycles.at[c, 'peak_value_ms'] = hperiod[ind_min_rate]
+
+
+    resphrv_cycles['decay_amplitude_bpm'] = resphrv_cycles['peak_value_bpm'] - resphrv_cycles['trough_value_bpm']
+    resphrv_cycles['rising_amplitude_ms'] = abs(resphrv_cycles['trough_value_ms'] - resphrv_cycles['peak_value_ms'])
+    resphrv_cycles['relative_decay_amplitude_bpm'] = resphrv_cycles['decay_amplitude_bpm'] / (resphrv_cycles['peak_value_bpm'] + resphrv_cycles['trough_value_bpm'])
+    resphrv_cycles['relative_rising_amplitude_ms'] = resphrv_cycles['rising_amplitude_ms'] / (resphrv_cycles['peak_value_ms'] + resphrv_cycles['peak_value_ms'])
+    resphrv_cycles.loc[resphrv_cycles.index[1:] ,'rising_amplitude_bpm'] = resphrv_cycles['peak_value_bpm'].values[1:] - resphrv_cycles['trough_value_bpm'].values[:-1]
+    resphrv_cycles.loc[resphrv_cycles.index[1:] ,'decay_amplitude_ms'] = abs(resphrv_cycles['trough_value_ms'].values[1:] - resphrv_cycles['peak_value_ms'].values[:-1])
+    resphrv_cycles.loc[resphrv_cycles.index[1:] ,'relative_rising_amplitude_bpm'] = resphrv_cycles.loc[resphrv_cycles.index[1:] ,'rising_amplitude_bpm'] / (resphrv_cycles['peak_value_bpm'].values[1:] + resphrv_cycles['trough_value_bpm'].values[:-1])
+    resphrv_cycles.loc[resphrv_cycles.index[1:] ,'relative_decay_amplitude_ms'] = resphrv_cycles.loc[resphrv_cycles.index[1:] ,'decay_amplitude_ms'] / (resphrv_cycles['trough_value_ms'].values[1:] + resphrv_cycles['peak_value_ms'].values[:-1])
+
+    mask = resphrv_cycles['decay_amplitude_bpm'] < 0
+    resphrv_cycles.loc[mask,'decay_amplitude_bpm'] = np.nan
+    resphrv_cycles.loc[mask,'relative_decay_amplitude_bpm'] = np.nan
+    resphrv_cycles.loc[mask,'rising_amplitude_ms'] = np.nan
+    resphrv_cycles.loc[mask,'relative_rising_amplitude_ms'] = np.nan
+    mask = resphrv_cycles['rising_amplitude_bpm'] < 0
+    resphrv_cycles.loc[mask,'rising_amplitude_bpm'] = np.nan
+    resphrv_cycles.loc[mask,'relative_rising_amplitude_bpm'] = np.nan
+    resphrv_cycles.loc[mask,'decay_amplitude_ms'] = np.nan
+    resphrv_cycles.loc[mask,'relative_decay_amplitude_ms'] = np.nan
+
+    resphrv_cycles.loc[resphrv_cycles.index[1:] ,'rising_duration_bpm'] = resphrv_cycles['peak_time_bpm'].values[1:] - resphrv_cycles['trough_time_bpm'].values[:-1]
+    resphrv_cycles.loc[resphrv_cycles.index[1:] ,'decay_duration_ms'] = resphrv_cycles['trough_time_ms'].values[1:] - resphrv_cycles['peak_time_ms'].values[:-1]
+    resphrv_cycles['decay_duration_bpm'] = resphrv_cycles['trough_time_bpm'] - resphrv_cycles['peak_time_bpm']
+    resphrv_cycles['rising_duration_ms'] = resphrv_cycles['peak_time_ms'] - resphrv_cycles['trough_time_ms']
+
+    resphrv_cycles['rising_slope_bpm'] = resphrv_cycles['rising_amplitude_bpm'] / resphrv_cycles['rising_duration_bpm']
+    resphrv_cycles['decay_slope_ms'] = resphrv_cycles['decay_amplitude_ms'] / resphrv_cycles['decay_duration_ms']
+    resphrv_cycles['decay_slope_bpm'] = resphrv_cycles['decay_amplitude_bpm'] / resphrv_cycles['decay_duration_bpm']
+    resphrv_cycles['rising_slope_ms'] = resphrv_cycles['rising_amplitude_ms'] / resphrv_cycles['rising_duration_ms']
+
+    resphrv_cycles['average_rate_bpm'] = (resphrv_cycles['peak_value_bpm'] + resphrv_cycles['trough_value_bpm']) / 2
+    resphrv_cycles['average_period_ms'] = (resphrv_cycles['peak_value_ms'] + resphrv_cycles['trough_value_ms']) / 2
+
+    if return_cyclic_cardiac_rate and return_cyclic_cardiac_period:
+        return resphrv_cycles, cyclic_cardiac_rate, cyclic_cardiac_period
+    elif not return_cyclic_cardiac_rate and return_cyclic_cardiac_period:
+        return resphrv_cycles, cyclic_cardiac_period
+    elif return_cyclic_cardiac_rate and not return_cyclic_cardiac_period:
+        return resphrv_cycles, cyclic_cardiac_rate
+    else:
+        return resphrv_cycles
 
 
 
--- tests/test_resphrv.py
+++ tests/test_resphrv.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pathlib import Path
 
-from physio import compute_respiration, compute_ecg, compute_resphrv
+from physio import compute_respiration, compute_ecg, compute_resphrv, compute_resphrv_rate_period
 
 # read signals
 example_folder = Path(__file__).parents[1] / 'examples'
@@ -20,10 +20,34 @@ def test_compute_resphrv():
 
 
 
-    resphrv_cycles, cyclic_cardiac_rate = compute_resphrv(resp_cycles, ecg_peaks, srate=10., two_segment=True)
-    print(resphrv_cycles)
+    # resphrv_cycles, cyclic_cardiac_rate = compute_resphrv(resp_cycles, ecg_peaks, srate=10., two_segment=True)
+    # print(resphrv_cycles)
+    # print(resphrv_cycles['decay_amplitude'].describe())
 
-    resphrv_cycles, cyclic_cardiac_rate_1seg = compute_resphrv(resp_cycles, ecg_peaks, srate=5., two_segment=False)
+    # resphrv_cycles, cyclic_cardiac_rate_1seg = compute_resphrv(resp_cycles, ecg_peaks, srate=5., two_segment=False)
+
+    # resphrv_cycles = compute_resphrv_rate_period(resp_cycles, ecg_peaks, return_cyclic_cardiac_rate = False, return_cyclic_cardiac_period = False)
+    # print(resphrv_cycles.describe())
+    # print(resphrv_cycles['decay_amplitude_bpm'].describe())
+    # print(resphrv_cycles[['decay_amplitude_bpm','rising_amplitude_ms']])
+    # print(resphrv_cycles.isna().sum())
+
+    # resphrv_cycles, cyclic_cardiac_rate = compute_resphrv_rate_period(resp_cycles, ecg_peaks, return_cyclic_cardiac_rate = True, return_cyclic_cardiac_period = False)
+    # print(cyclic_cardiac_rate)
+    # print(resphrv_cycles['decay_amplitude_bpm'].values)
+    # print(np.ptp(cyclic_cardiac_rate, axis = 1))
+
+    resphrv_cycles, cyclic_cardiac_rate, cyclic_cardiac_period = compute_resphrv_rate_period(resp_cycles, ecg_peaks, return_cyclic_cardiac_rate = True, return_cyclic_cardiac_period = True)
+    print(resphrv_cycles)
+    print(cyclic_cardiac_rate)
+    # print(resphrv_cycles['decay_amplitude_bpm'].values)
+    # print(np.ptp(cyclic_cardiac_rate, axis = 1))
+
+    print(cyclic_cardiac_period)
+    # print(resphrv_cycles['rising_amplitude_ms'].values)
+    # print(np.ptp(cyclic_cardiac_period, axis = 1))
+    # print(resphrv_cycles['average_period_ms'].values.round(0))
+    # print(np.median(cyclic_cardiac_period, axis = 1).round(0))
 
 
     # import matplotlib.pyplot as plt
